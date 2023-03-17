84da1292855641c1074d9e0965e1a98e8a4e7c71
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2023-03-17 19:30:40 -0400

    Add brute force solution

---
+++ brute_force_bjk.py
@@ -0,0 +1,33 @@
+from typing import List
+
+list_variations: List[str] = []
+
+def brute_force(a : str, b: str) -> str:
+   generate_VWR('()', 2 * max(len(a), len(b)))
+   for variation in list_variations:
+      if contains(variation, a, b):
+         return variation
+
+def contains(chain: str, a: str, b: str) -> bool:
+   current_a, current_b = 0, 0
+   for item in chain:
+      if current_a < len(a) and a[current_a] == item:
+         current_a += 1
+      if current_b < len(b) and b[current_b] == item:
+         current_b += 1
+   return current_a == len(a) and current_b == len(b)
+
+def generate_VWR(alphabet : str, size: int) -> None:
+   for i in range(1, size + 1):
+      generator_VWR(alphabet, [None] * i, 0)
+
+def generator_VWR(alphabet: str, variations : List[str], pos : int) -> None:
+   if pos == len(variations):
+      variation = ''
+      for i in range(len(variations)):
+         variation += variations[i]
+      list_variations.append(variation)
+   else:
+      for i in range(len(alphabet)):
+         variations[pos] = alphabet[i]
+         generator_VWR(alphabet, variations, pos + 1)
---
+++ tester.py
@@ -0,0 +1,3 @@
+from brute_force_bjk import brute_force
+
+print(brute_force('((', '))'))
